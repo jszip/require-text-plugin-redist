1f15d485f633bde30f9cb49dd75b2be1210e2378
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-02-22 12:05:19 +0000

    Update update-check

--- update-check.rb
+++ update-check.rb
@@ -7,10 +7,13 @@ require 'open-uri'
 
 current = Gem::Version.new(JSON.parse(open('https://github.com/requirejs/text/raw/master/package.json').read)["version"])
 
-maven = Gem::Version.new(Nokogiri::XML(File.open("pom.xml")).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+maven = Gem::Version.new(Nokogiri::XML(File.open(File.expand_path('../pom.xml', __FILE__))).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+
+puts "["+File.dirname(__FILE__)+"]   Next redist: " + maven.to_s
+puts "["+File.dirname(__FILE__)+"] Last upstream: " + current.to_s
 
 if current >= maven
-	puts "A release can be made"
+	puts "["+File.dirname(__FILE__)+"] A release can be made!!!"
 	exit 1 
 end
-puts "Everything up to date"
+puts "["+File.dirname(__FILE__)+"] Everything up to date."
